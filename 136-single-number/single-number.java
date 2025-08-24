20a609a4674453ff2826ada777289a256f03bcb6
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-08-24 12:14:11 +0530

    Time: 13 ms (19.29%) | Memory: 44.8 MB (96.99%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         HashMap<Integer,Integer> map = new HashMap<>();
+        
+        // Count frequencies
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        // Find the element with frequency 1
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1){
+                return entry.getKey();
+            }
+        }
+        
+        return -1; 
+    }
+}
