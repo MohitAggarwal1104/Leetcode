9a57868b863d41fab086334b94913d985bf5f555
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-09-10 12:31:18 +0530

    Time: 1 ms (52.36%) | Memory: 41 MB (46.48%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n<=0)return false;
-        int prime[]={2,3,5};
-        for(int p:prime){
-            while(n%p==0){
-                n=n/p;
-            }
-        }
-        return n==1;
+         if (n <= 0) return false; 
+        
+
+        while (n % 2 == 0) n /= 2;
+        while (n % 3 == 0) n /= 3;
+        while (n % 5 == 0) n /= 5;
+
+        return n == 1;
     }
 }
