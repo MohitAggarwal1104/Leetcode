245ab32c3d71166e03b409b3657a0f47661fc3d4
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-09-10 18:49:38 +0530

    Time: 1 ms (89.81%) | Memory: 44.7 MB (91.59%) - LeetSync

--- 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.java
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.java
@@ -1,16 +1,31 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
-        ArrayList<Integer>list=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(i%2==0){
-                int freq=nums[i];
-                int val=nums[i+1];
-                for(int j=0;j<freq;j++)list.add(val);
-            }
+        // ArrayList<Integer>list=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(i%2==0){
+        //         int freq=nums[i];
+        //         int val=nums[i+1];
+        //         for(int j=0;j<freq;j++)list.add(val);
+        //     }
+        // }
+        // int arr[]=new int[list.size()];
+        // int count=0;
+        // for(int i : list)arr[count++]=i;
+        // return arr;
+        int size = 0;
+    for (int i = 0; i < nums.length; i+=2) {
+        size += nums[i];
+    }
+    int [] result = new int [size];
+    int index = 0;
+    for (int i = 0; i < nums.length; i+=2) {
+        int freq = nums[i];
+        int val = nums[i + 1];
+        for (int j = 0; j < freq; j++) {
+            result[index] = val;
+            index++;
         }
-        int arr[]=new int[list.size()];
-        int count=0;
-        for(int i : list)arr[count++]=i;
-        return arr;
+    }
+    return result;
     }
 }
