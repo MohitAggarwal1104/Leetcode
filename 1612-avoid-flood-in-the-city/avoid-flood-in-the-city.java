7bbc309844bc27d6c9872d91c9e1336f83583416
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-10-07 21:20:02 +0530

    Time: 63 ms (94.52%) | Memory: 61.6 MB (74.89%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -1,16 +1,61 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] avoidFlood(int[] rains) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         int[] arr = new int[rains.length];
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         for (int i = 0; i < rains.length; i++) {
+//             int lake = rains[i];
+
+//             if (lake == 0) {
+//                 list.add(i);
+//                 arr[i] = 1;
+//                 continue;
+//             }
+
+//             arr[i] = -1;
+
+//             if (map.containsKey(lake)) {
+//                 int lastRainDay = map.get(lake);
+//                 int dryDayIndex = -1;
+
+//                 for (int j : list) {
+//                     if (j > lastRainDay) {
+//                         dryDayIndex = j;
+//                         break;
+//                     }
+//                 }
+
+//                 if (dryDayIndex == -1) {
+//                     return new int[0];
+//                 }
+
+//                 arr[dryDayIndex] = lake;
+//                 list.remove(Integer.valueOf(dryDayIndex));
+//                 map.remove(lake);
+//             }
+
+//             map.put(lake, i);
+//         }
+
+//         return arr;
+//     }
+// }
 import java.util.*;
 
 class Solution {
     public int[] avoidFlood(int[] rains) {
         Map<Integer, Integer> map = new HashMap<>();
+        TreeSet<Integer> dryDays = new TreeSet<>();
         int[] arr = new int[rains.length];
-        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < rains.length; i++) {
             int lake = rains[i];
 
             if (lake == 0) {
-                list.add(i);
+                dryDays.add(i);
                 arr[i] = 1;
                 continue;
             }
@@ -19,22 +64,14 @@ class Solution {
 
             if (map.containsKey(lake)) {
                 int lastRainDay = map.get(lake);
-                int dryDayIndex = -1;
+                Integer dryDayIndex = dryDays.higher(lastRainDay);
 
-                for (int j : list) {
-                    if (j > lastRainDay) {
-                        dryDayIndex = j;
-                        break;
-                    }
-                }
-
-                if (dryDayIndex == -1) {
+                if (dryDayIndex == null) {
                     return new int[0];
                 }
 
                 arr[dryDayIndex] = lake;
-                list.remove(Integer.valueOf(dryDayIndex));
-                map.remove(lake);
+                dryDays.remove(dryDayIndex);
             }
 
             map.put(lake, i);
