70df9b1143ecf66a4f1546f26a3471a903550a00
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-10-10 10:58:32 +0530

    Time: 2 ms (97.93%) | Memory: 59.5 MB (86.90%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -11,17 +11,32 @@
 //         return max;
 //     }
 // }
+// class Solution {
+//     public int maximumEnergy(int[] energy, int k) {
+//         int n = energy.length;
+//         int[] dp = new int[n];
+//         int max = Integer.MIN_VALUE;
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             dp[i] = energy[i] + (i + k < n ? dp[i + k] : 0);
+//             max = Math.max(max, dp[i]);
+//         }
+
+//         return max;
+//     }
+// }
 class Solution {
     public int maximumEnergy(int[] energy, int k) {
-        int n = energy.length;
-        int[] dp = new int[n];
-        int max = Integer.MIN_VALUE;
-
-        for (int i = n - 1; i >= 0; i--) {
-            dp[i] = energy[i] + (i + k < n ? dp[i + k] : 0);
-            max = Math.max(max, dp[i]);
+        int n = energy.length, m = n-k, max = -1000;
+        for (int i = m; i < n; i++) {
+            for (int j = i, tmp = 0; j > -1; j -= k) {
+                tmp += energy[j];
+                if (max < tmp) {
+                    max = tmp;
+                }
+            }
         }
-
+        
         return max;
     }
 }
