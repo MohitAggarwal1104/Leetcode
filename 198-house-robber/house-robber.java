dd0c6ce7eeb69a60f9452988ada116e5258c3e47
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-10-15 12:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (89.67%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,13 +1,17 @@
 class Solution {
+    public int robHelper(int index,int nums[],int dp[]){
+        if(index==0)return nums[index];
+        if(index<0)return 0;
+        if(dp[index]!=-1)return dp[index];
+        int pick=nums[index]+robHelper(index-2,nums,dp);
+        int notPick=0+robHelper(index-1,nums,dp);
+        dp[index]=Math.max(pick,notPick);
+        return Math.max(pick,notPick);
+    }
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        int prev = 0; // max up to i-1
-        int curr = 0; // max up to i
-        for (int n : nums) {
-            int next = Math.max(curr, prev + n);
-            prev = curr;
-            curr = next;
-        }
-        return curr;
+        int dp[]=new int[nums.length];
+        Arrays.fill(dp, -1);
+        return robHelper(nums.length-1,nums,dp);
+
     }
 }
