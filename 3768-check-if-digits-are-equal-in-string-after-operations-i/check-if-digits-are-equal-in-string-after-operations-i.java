9a2e6a4e3547fc28901f54cd4a4f40168320a3e4
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-10-23 14:03:02 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (93.09%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,13 +1,30 @@
+// class Solution {
+//     public boolean hasSameDigits(String s) {
+//         while (s.length() > 2) {
+//             StringBuilder sb = new StringBuilder();
+//             for (int i = 0; i < s.length() - 1; i++) {
+//                 int sum = (s.charAt(i) - '0' + s.charAt(i + 1) - '0') % 10;
+//                 sb.append(sum);
+//             }
+//             s = sb.toString();
+//         }
+//         return s.charAt(0) == s.charAt(1);
+//     }
+// }
 class Solution {
-    public boolean hasSameDigits(String s) {
-        while (s.length() > 2) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < s.length() - 1; i++) {
-                int sum = (s.charAt(i) - '0' + s.charAt(i + 1) - '0') % 10;
-                sb.append(sum);
-            }
-            s = sb.toString();
-        }
-        return s.charAt(0) == s.charAt(1);
+  public boolean hasSameDigits(String s) {
+    int[] arr = new int[s.length()];
+
+    for (int i = 0; i < arr.length; i++)
+      arr[i] = s.charAt(i) - '0';
+    
+    for (int length = arr.length; length > 2; length--) {
+      for (int i = 0; i < length - 1; i++) {
+        arr[i] = (arr[i] + arr[i + 1]) % 10;
+      }
     }
+
+    return arr[0] == arr[1];
+      
+  }
 }
