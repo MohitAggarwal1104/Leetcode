423fa10723520f9f7107b3874f881fc863248322
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-10-30 14:58:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (82.73%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -2,17 +2,18 @@ class Solution {
     public int minAddToMakeValid(String s) {
         Stack<Character> st=new Stack<>();
         int count=0;
+        int n=0;
         for(char j : s.toCharArray()){
-            if(j=='(')st.push('(');
+            if(j=='(')n++;
             else {
-                if(st.isEmpty()){
+                if(n==0){
                     count++;
                 }
                 else{
-                    st.pop();
+                    n--;
                 }
             }
         }
-        return count+st.size();
+        return count+n;
     }
 }
