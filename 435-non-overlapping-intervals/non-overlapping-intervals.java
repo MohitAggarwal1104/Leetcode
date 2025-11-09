e10eb40789fa68eb20c381e6b4ef6781e527f886
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-11-09 16:13:07 +0530

    Time: 44 ms (99.51%) | Memory: 116 MB (5.25%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,32 +1,53 @@
-import java.util.*;
+// import java.util.*;
 
-class Pair {
-    int start;
-    int end;
-    Pair(int s, int e) {
-        start = s;
-        end = e;
-    }
-}
+// class Pair {
+//     int start;
+//     int end;
+//     Pair(int s, int e) {
+//         start = s;
+//         end = e;
+//     }
+// }
+
+// class Solution {
+//     public int eraseOverlapIntervals(int[][] intervals) {
+//         if (intervals.length == 0) return 0;
+
+//         ArrayList<Pair> list = new ArrayList<>();
+//         for (int[] i : intervals) {
+//             list.add(new Pair(i[0], i[1]));
+//         }
+
+//         Collections.sort(list, (a, b) -> a.end - b.end);
+
+//         int count = 0;
+//         int prevEnd = Integer.MIN_VALUE;
+
+//         for (Pair p : list) {
+//             if (p.start >= prevEnd) {
+//                 count++;
+//                 prevEnd = p.end;
+//             }
+//         }
+
+//         return intervals.length - count;
+//     }
+// }
+import java.util.*;
 
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         if (intervals.length == 0) return 0;
 
-        ArrayList<Pair> list = new ArrayList<>();
-        for (int[] i : intervals) {
-            list.add(new Pair(i[0], i[1]));
-        }
-
-        Collections.sort(list, (a, b) -> a.end - b.end);
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
 
         int count = 0;
         int prevEnd = Integer.MIN_VALUE;
 
-        for (Pair p : list) {
-            if (p.start >= prevEnd) {
+        for (int[] interval : intervals) {
+            if (interval[0] >= prevEnd) {
                 count++;
-                prevEnd = p.end;
+                prevEnd = interval[1];
             }
         }
 
