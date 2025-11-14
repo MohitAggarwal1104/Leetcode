329556510095f108cbe5d4f08f573e3d78a88294
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-11-14 10:03:36 +0530

    Time: 5 ms (51.20%) | Memory: 45.4 MB (68.79%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -26,21 +26,39 @@
 // }
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        // int n = triangle.size();
+        // int dp[][] = new int[n][n];
+
+        // for (int j = 0; j < n; j++) {
+        //     dp[n - 1][j] = triangle.get(n - 1).get(j);
+        // }
+
+        // for (int i = n - 2; i >= 0; i--) {
+        //     for (int j = 0; j <= i; j++) {
+        //         int down = triangle.get(i).get(j) + dp[i + 1][j];
+        //         int diagonal = triangle.get(i).get(j) + dp[i + 1][j + 1];
+        //         dp[i][j] = Math.min(down, diagonal);
+        //     }
+        // }
+
+        // return dp[0][0];
         int n = triangle.size();
-        int dp[][] = new int[n][n];
+        int dp[] = new int[n];
 
         for (int j = 0; j < n; j++) {
-            dp[n - 1][j] = triangle.get(n - 1).get(j);
+            dp[j] = triangle.get(n - 1).get(j);
         }
 
         for (int i = n - 2; i >= 0; i--) {
+            int temp[] = new int[n];
             for (int j = 0; j <= i; j++) {
-                int down = triangle.get(i).get(j) + dp[i + 1][j];
-                int diagonal = triangle.get(i).get(j) + dp[i + 1][j + 1];
-                dp[i][j] = Math.min(down, diagonal);
+                int down = triangle.get(i).get(j) + dp[j];
+                int diagonal = triangle.get(i).get(j) + dp[j + 1];
+                temp[j] = Math.min(down, diagonal);
             }
+            dp=temp;
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 }
