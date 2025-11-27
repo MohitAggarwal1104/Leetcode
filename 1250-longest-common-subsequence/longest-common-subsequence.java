36642e1caf5c2772c85611a018f2e7d4b8795db9
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-11-27 14:34:32 +0530

    Time: 20 ms (75.89%) | Memory: 42.7 MB (96.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -10,21 +10,38 @@ class Solution {
         // for(int i=0;i<text1.length();i++)Arrays.fill(dp[i],-1);
         // return count(text1,text2,text1.length()-1,text2.length()-1,dp);
 
-        int dp[][]=new int[text1.length()+1][text2.length()+1];
-        for(int i=0;i<=text1.length();i++)Arrays.fill(dp[i],-1);
-        for(int i=0;i<=text1.length();i++){
-            dp[i][0]=0;
-        }
+        // int dp[][]=new int[text1.length()+1][text2.length()+1];
+        // for(int i=0;i<=text1.length();i++)Arrays.fill(dp[i],-1);
+        // for(int i=0;i<=text1.length();i++){
+        //     dp[i][0]=0;
+        // }
+        // for(int i=0;i<=text2.length();i++){
+        //     dp[0][i]=0;
+        // }
+
+        // for(int i=1;i<=text1.length();i++){
+        //     for(int j=1;j<=text2.length();j++){
+        //         if(text1.charAt(i-1)==(text2.charAt(j-1)))dp[i][j]= 1+dp[i-1][j-1];
+        //         else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+        //     }
+        // }
+        // return dp[text1.length()][text2.length()];
+        
+        int dp[]=new int[text2.length()+1];
+        Arrays.fill(dp,-1);
         for(int i=0;i<=text2.length();i++){
-            dp[0][i]=0;
+            dp[i]=0;
         }
 
         for(int i=1;i<=text1.length();i++){
+            int prev = 0; 
             for(int j=1;j<=text2.length();j++){
-                if(text1.charAt(i-1)==(text2.charAt(j-1)))dp[i][j]= 1+dp[i-1][j-1];
-                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                 int temp = dp[j];
+                if(text1.charAt(i-1)==(text2.charAt(j-1))) dp[j] = 1 + prev;
+                else dp[j]=Math.max(dp[j],dp[j-1]);
+                prev = temp;
             }
         }
-        return dp[text1.length()][text2.length()];
+        return dp[text2.length()];
     }
 }
