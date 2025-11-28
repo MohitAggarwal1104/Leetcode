dae7484cc01416919a68fb064aa5c12b1fc7b7e7
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-11-28 15:40:36 +0530

    Time: 14 ms (91.93%) | Memory: 42.9 MB (91.60%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -13,28 +13,42 @@ class Solution {
         // int dp[][]=new int[s.length()][t.length()];
         // for(int i=0;i<s.length();i++)Arrays.fill(dp[i],-1);
         // return count(s.length()-1,t.length()-1,s,t,dp);
-        int n = s.length();
+        // int n = s.length();
+        // int m = t.length();
+
+        // int dp[][] = new int[n+1][m+1];
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = 1;
+        // }
+
+        // // dp[0][j > 0] will be 0 automatically
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(s.charAt(i-1) == t.charAt(j-1)){
+        //             dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+        //         } else {
+        //             dp[i][j] = dp[i-1][j];
+        //         }
+        //     }
+        // }
+
+        // return dp[n][m];
+         int n = s.length();
         int m = t.length();
 
-        int dp[][] = new int[n+1][m+1];
+        int dp[] = new int[m+1];
+        
+        dp[0] = 1;   // empty t is always matched
 
-        // When t is empty, there is exactly 1 subsequence in s that matches ""
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-        }
-
-        // dp[0][j > 0] will be 0 automatically
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+        for(int i=1; i<=n; i++){
+            for(int j=m; j>=1; j--){
                 if(s.charAt(i-1) == t.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j];
+                    dp[j] = dp[j] + dp[j-1];
                 }
             }
         }
 
-        return dp[n][m];
+        return dp[m];
     }
 }
