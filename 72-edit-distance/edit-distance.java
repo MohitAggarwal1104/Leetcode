008f0e013c6246ddccac3c6ab8710660830a879a
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-11-29 14:54:07 +0530

    Time: 5 ms (59.84%) | Memory: 47.1 MB (19.73%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -14,11 +14,34 @@ class Solution {
         }
     }
     public int minDistance(String word1, String word2) {
+        // int n=word1.length();
+        // int m=word2.length();
+        // int dp[][]=new int[n][m];
+        // for(int i=0;i<n;i++)Arrays.fill(dp[i],-1);
+        // return count(n-1,m-1,word1,word2,dp);
+
+        // Tabulation
         int n=word1.length();
         int m=word2.length();
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++)Arrays.fill(dp[i],-1);
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)Arrays.fill(dp[i],-1);
+        for(int i=0;i<=m;i++)dp[0][i]=i;
+        for(int i=0;i<=n;i++)dp[i][0]=i;
 
-        return count(n-1,m-1,word1,word2,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+            if(word1.charAt(i-1)==word2.charAt(j-1)){
+            dp[i][j]= dp[i-1][j-1];
+        }
+        else{
+            int insert=1+dp[i][j-1];
+            int remove=1+dp[i-1][j];
+            int replace=1+dp[i-1][j-1];
+            dp[i][j]= Math.min(insert,Math.min(remove,replace));
+        }
+            }
+        }
+        return dp[n][m];
+
     }
 }
