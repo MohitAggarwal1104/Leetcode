29d1cf3d4773fc05e5c55c042d1beaf63c024be8
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-12-04 13:29:35 +0530

    Time: 1 ms (73.02%) | Memory: 43.7 MB (9.91%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -17,13 +17,28 @@ class Solution {
         // for(int i=0;i<n;i++){Arrays.fill(dp[i],-1);}
         // return maxmoney(0,1,prices,dp);
 
+        // int n=prices.length;
+        // int dp[][]=new int[n+2][2];
+
+        // for(int i=n-1;i>=0;i--){
+        //     dp[i][1]=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+        //     dp[i][0]=Math.max((prices[i]+dp[i+2][1]),dp[i+1][0]);
+        // }
+        // return dp[0][1];
+
         int n=prices.length;
-        int dp[][]=new int[n+2][2];
+        int front1[]=new int[2];
+        int front2[]=new int[2];
+        int curr[]=new int[2];
 
         for(int i=n-1;i>=0;i--){
-            dp[i][1]=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
-            dp[i][0]=Math.max((prices[i]+dp[i+2][1]),dp[i+1][0]);
+            curr[1]=Math.max(-prices[i]+front1[0],front1[1]);
+            curr[0]=Math.max((prices[i]+front2[1]),front1[0]);
+
+            front2 = front1.clone();
+            front1 = curr.clone();
+
         }
-        return dp[0][1];
+        return curr[1];
     }
 }
