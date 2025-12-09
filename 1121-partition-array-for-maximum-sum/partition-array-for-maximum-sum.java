c91eb3f1248831cd472abfa06ddb04980c39c09b
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-12-09 09:37:09 +0530

    Time: 6 ms (94.07%) | Memory: 43.9 MB (23.56%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int f(int ind,int arr[],int dp[],int k,int n){
+        if(ind==n)return 0;
+        if(dp[ind]!=-1)return dp[ind];
+
+        int length=0;int maxi=(int)-1e9;int maxans=(int)-1e9;
+        for(int j=ind;j<Math.min(n,ind+k);j++){
+            length++;
+            maxi=Math.max(maxi,arr[j]);
+            int sum=length*maxi+f(j+1,arr,dp,k,n);
+            maxans=Math.max(maxans,sum);
+        }
+        return dp[ind]=maxans;
+    }
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n=arr.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return f(0,arr,dp,k,n);
+    }
+}
