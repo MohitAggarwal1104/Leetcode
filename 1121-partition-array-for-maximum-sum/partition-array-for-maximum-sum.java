113691029ef665c00b92b12ba31b6caf1fbb5918
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-12-09 09:47:28 +0530

    Time: 6 ms (94.07%) | Memory: 43.8 MB (47.12%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -3,7 +3,7 @@ class Solution {
         if(ind==n)return 0;
         if(dp[ind]!=-1)return dp[ind];
 
-        int length=0;int maxi=(int)-1e9;int maxans=(int)-1e9;
+        int length=0;int maxi=-1;int maxans=-1;
         for(int j=ind;j<Math.min(n,ind+k);j++){
             length++;
             maxi=Math.max(maxi,arr[j]);
@@ -13,24 +13,24 @@ class Solution {
         return dp[ind]=maxans;
     }
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        // int n=arr.length;
-        // int dp[]=new int[n];
-        // Arrays.fill(dp,-1);
-        // return f(0,arr,dp,k,n);
-
         int n=arr.length;
-        int dp[]=new int[n+1];
-        dp[n]=0;
-        for(int ind=n-1;ind>=0;ind--){
-                int length=0;int maxi=(int)-1e9;int maxans=(int)-1e9;
-                for(int j=ind;j<Math.min(n,ind+k);j++){
-                    length++;
-                    maxi=Math.max(maxi,arr[j]);
-                    int sum=length*maxi+dp[j+1];
-                    maxans=Math.max(maxans,sum);
-                }
-                dp[ind]=maxans;
-        }
-        return dp[0];
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return f(0,arr,dp,k,n);
+
+        // int n=arr.length;
+        // int dp[]=new int[n+1];
+        // dp[n]=0;
+        // for(int ind=n-1;ind>=0;ind--){
+        //         int length=0;int maxi=(int)-1e9;int maxans=(int)-1e9;
+        //         for(int j=ind;j<Math.min(n,ind+k);j++){
+        //             length++;
+        //             maxi=Math.max(maxi,arr[j]);
+        //             int sum=length*maxi+dp[j+1];
+        //             maxans=Math.max(maxans,sum);
+        //         }
+        //         dp[ind]=maxans;
+        // }
+        // return dp[0];
     }
 }
