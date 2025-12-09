6c9f8b1ee01d2954f48259aa05d4a5cd25feeefd
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-12-09 12:27:44 +0530

    Time: 4 ms (92.25%) | Memory: 46.2 MB (30.71%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -9,54 +9,79 @@ class Solution {
         }
         return dp[i][prev+1]=len;
     }
+    // public int lengthOfLIS(int[] nums) {
+    //     // int n=nums.length;
+    //     // int dp[][]=new int[n][n+1];
+    //     // for(int i=0;i<n;i++)Arrays.fill(dp[i],-1);
+    //     // return f(0,-1,nums,dp);
+
+    //     // int n=nums.length;
+    //     // int dp[][]=new int[n+1][n+1];
+    //     // for(int i=n-1;i>=0;i--){
+    //     //     for(int prev=i-1;prev>=-1;prev--){
+    //     //         int len=dp[i+1][prev+1];// prev+1 due to co ordinates shieft
+    //     //         if(prev==-1 || nums[i]>nums[prev]){
+    //     //             len=Math.max(len,1+dp[i+1][i+1]);
+    //     //         }
+    //     //         dp[i][prev+1]=len;
+    //     //     }
+    //     // }
+    //     // return dp[0][0];
+
+    //     // int n=nums.length;
+    //     // // int dp[]=new int[n+1];
+    //     // int curr[]=new int[n+1];
+    //     // for(int i=n-1;i>=0;i--){
+    //     //     for(int prev=i-1;prev>=-1;prev--){
+    //     //         int len=curr[prev+1];// prev+1 due to co ordinates shieft
+    //     //         if(prev==-1 || nums[i]>nums[prev]){
+    //     //             len=Math.max(len,1+curr[i+1]);
+    //     //         }
+    //     //         curr[prev+1]=len;
+    //     //     }
+    //     // //    dp=curr;
+    //     // }
+    //     // return curr[0];
+
+    //     // simple tabulation form
+
+    //     int n=nums.length;
+    //     int dp[]=new int[n];
+    //     Arrays.fill(dp,1);
+    //     int max=0;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<=i-1;j++){
+    //             if(nums[j]<nums[i]){
+    //                 dp[i]=Math.max(1+dp[j],dp[i]);
+    //             }
+    //         }
+    //         max=Math.max(max,dp[i]);
+    //     }
+    //     return max;
+    //     }
     public int lengthOfLIS(int[] nums) {
-        // int n=nums.length;
-        // int dp[][]=new int[n][n+1];
-        // for(int i=0;i<n;i++)Arrays.fill(dp[i],-1);
-        // return f(0,-1,nums,dp);
+        ArrayList<Integer> tails = new ArrayList<>();
 
-        // int n=nums.length;
-        // int dp[][]=new int[n+1][n+1];
-        // for(int i=n-1;i>=0;i--){
-        //     for(int prev=i-1;prev>=-1;prev--){
-        //         int len=dp[i+1][prev+1];// prev+1 due to co ordinates shieft
-        //         if(prev==-1 || nums[i]>nums[prev]){
-        //             len=Math.max(len,1+dp[i+1][i+1]);
-        //         }
-        //         dp[i][prev+1]=len;
-        //     }
-        // }
-        // return dp[0][0];
+        for (int num : nums) {
+            int pos = binarySearch(tails, num);
 
-        // int n=nums.length;
-        // // int dp[]=new int[n+1];
-        // int curr[]=new int[n+1];
-        // for(int i=n-1;i>=0;i--){
-        //     for(int prev=i-1;prev>=-1;prev--){
-        //         int len=curr[prev+1];// prev+1 due to co ordinates shieft
-        //         if(prev==-1 || nums[i]>nums[prev]){
-        //             len=Math.max(len,1+curr[i+1]);
-        //         }
-        //         curr[prev+1]=len;
-        //     }
-        // //    dp=curr;
-        // }
-        // return curr[0];
-
-        // simple tabulation form
-
-        int n=nums.length;
-        int dp[]=new int[n];
-        Arrays.fill(dp,1);
-        int max=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i-1;j++){
-                if(nums[j]<nums[i]){
-                    dp[i]=Math.max(1+dp[j],dp[i]);
-                }
+            if (pos == tails.size()) {
+                tails.add(num);     // extend the LIS
+            } else {
+                tails.set(pos, num); // replace with smaller ending
             }
-            max=Math.max(max,dp[i]);
         }
-        return max;
+
+        return tails.size();
+    }
+
+    private int binarySearch(ArrayList<Integer> arr, int target) {
+        int l = 0, r = arr.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr.get(mid) < target) l = mid + 1;
+            else r = mid - 1;
         }
+        return l;  // correct position
+    }
 }
