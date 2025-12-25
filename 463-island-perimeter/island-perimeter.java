94ed8230f0c43d2876a707b42feeecb1a6895e94
MohitAggarwal1104 <157101945+MohitAggarwal1104@users.noreply.github.com>
2025-12-25 15:36:23 +0530

    Time: 7 ms (23.84%) | Memory: 47.1 MB (68.39%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,25 +1,46 @@
 public class Solution {
+
+    int n, m;
+    int[][] grid;
+    boolean[][] visited;
+
     public int islandPerimeter(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int cnt = 0;
+        this.grid = grid;
+        n = grid.length;
+        m = grid[0].length;
+        visited = new boolean[n][m];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    if ((j > 0 && grid[i][j - 1] == 0) || j == 0)
-                        cnt++;
-
-                    if ((i > 0 && grid[i - 1][j] == 0) || i == 0)
-                        cnt++;
-
-                    if ((j < m - 1 && grid[i][j + 1] == 0) || j == m - 1)
-                        cnt++;
-
-                    if ((i < n - 1 && grid[i + 1][j] == 0) || i == n - 1)
-                        cnt++;
+                    return dfs(i, j); // start DFS from first land cell
                 }
             }
         }
-        return cnt;
+        return 0;
+    }
+
+    private int dfs(int i, int j) {
+        // Out of boundary
+        if (i < 0 || j < 0 || i >= n || j >= m)
+            return 1;
+
+        // Water cell
+        if (grid[i][j] == 0)
+            return 1;
+
+        // Already visited land
+        if (visited[i][j])
+            return 0;
+
+        visited[i][j] = true;
+
+        int perimeter = 0;
+        perimeter += dfs(i, j - 1); // left
+        perimeter += dfs(i - 1, j); // up
+        perimeter += dfs(i, j + 1); // right
+        perimeter += dfs(i + 1, j); // down
+
+        return perimeter;
     }
 }
